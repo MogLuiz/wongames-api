9e2f923222a4816bf00cc786bf1dba137567b71f
Luiz Henrique <luizhjramos@outlook.com>
2022-03-10 20:35:38 -0300

    feat: strapi API initialize

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c6ec41bd8ac3e3906ea121451fadbac7'),
+    },
+  },
+});
