7243dd056a32f37998e4e26c338b9999164dd183
Luiz Henrique <luizhjramos@outlook.com>
2022-03-14 09:09:18 -0300

    feat: Custom won game login page

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoWon from '../../../../assets/images/logo-won-dark.svg';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoWon} alt="Won Games Logo" />;
+
+export default Logo;
