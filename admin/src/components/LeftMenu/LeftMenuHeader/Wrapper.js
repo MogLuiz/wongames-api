5f135e03c2b25a437fc5f72547b7bcde2e997c76
Luiz Henrique <luizhjramos@outlook.com>
2022-03-14 09:42:14 -0300

    feat: won games pallet theme

--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -6,7 +6,6 @@ import Logo from '../../../assets/images/logo-won.svg';
 const Wrapper = styled.div`
   background-color: ${(props) =>
     props.theme.main.colors.leftMenu['background-header-link']};
-  padding-left: 2rem;
   height: ${props => props.theme.main.sizes.leftMenu.height};
 
   .leftMenuHeaderLink {
@@ -17,16 +16,12 @@ const Wrapper = styled.div`
 
   .projectName {
     display: block;
-    width: 100%;
     height: ${props => props.theme.main.sizes.leftMenu.height};
-    font-size: 2rem;
-    letter-spacing: 0.2rem;
-    color: $white;
 
     background-image: url(${Logo});
     background-repeat: no-repeat;
-    background-position: left center;
-    background-size: auto 2.5rem;
+    background-position: center center;
+    background-size: 12rem;
   }
 `;
 
