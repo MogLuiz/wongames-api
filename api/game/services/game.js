0eeae602ad687bb0a70ff12ebdce1c3e9d6e68ff
Luiz Henrique <luizhjramos@outlook.com>
2022-04-25 22:32:51 -0300

    feat(services): consuming the gog games API

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+const axios = require("axios");
+
+module.exports = {
+  populate: async (params) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(products);
+  },
+};
