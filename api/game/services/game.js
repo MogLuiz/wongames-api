aa317a1de88e37f64c92c0b35e323e2655329c3a
Luiz Henrique <luizhjramos@outlook.com>
2022-04-25 22:57:53 -0300

    feat(services): scrapper game data with jsdom

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,6 +7,21 @@
 
 const axios = require("axios");
 
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -15,6 +30,6 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products);
+    console.log(await getGameInfo(products[0].slug));
   },
 };
