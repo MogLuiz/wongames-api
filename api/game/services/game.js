e445cc7d3ddca73e0520e5f4b2a70f54b03e9fbd
Luiz Henrique <luizhjramos@outlook.com>
2022-04-27 18:53:21 -0300

    feat(services): createManyToManyData function

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -39,6 +39,35 @@ async function create(name, entityName) {
   }
 }
 
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((item) => {
+        categories[item] = true;
+      });
+    supportedOperatingSystems &&
+      supportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -49,11 +78,7 @@ module.exports = {
 
     console.log(products[0]);
 
-    await strapi.services.publisher.create({
-      name: products[0].publisher,
-      slug: slugify(products[0].publisher).toLowerCase(),
-    });
-
-    // console.log(await getGameInfo(products[0].slug));
+    await create(products[2].publisher, "publisher");
+    await create(products[2].developer, "developer");
   },
 };
