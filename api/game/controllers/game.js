55ab9e79276fe703ea996eaf7cc4932b9a5dc689
Luiz Henrique <luizhjramos@outlook.com>
2022-04-27 21:12:08 -0300

    feat(services): customizing the query params API call

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -7,9 +7,15 @@
 
 module.exports = {
   populate: async (ctx) => {
-    console.log("initializing");
+    console.log("Starting to populate...");
 
-    await strapi.services.game.populate();
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+
+    await strapi.services.game.populate(options);
 
     ctx.send("Finished populating!");
   },
--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,6 +7,7 @@
 
 const axios = require("axios");
 const slugify = require("slugify");
+const qs = require("querystring");
 
 function Exception(e) {
   return { e, data: e.data && e.data.errors && e.data.errors };
@@ -164,14 +165,16 @@ async function createGames(products) {
 module.exports = {
   populate: async (params) => {
     try {
-      const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+      const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&${qs.stringify(
+        params
+      )}`;
 
       const {
         data: { products },
       } = await axios.get(gogApiUrl);
 
-      await createManyToManyData([products[2], products[3]]);
-      await createGames([products[2], products[3]]);
+      await createManyToManyData(products);
+      await createGames(products);
     } catch (e) {
       console.log("populate", Exception(e));
     }
